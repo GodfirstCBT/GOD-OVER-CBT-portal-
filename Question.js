b1349563cfd52173664e34c43f3bbf1f0b6dd0ad
GodfirstCBT <sundayajoga412@gmail.com>
2026-06-30 05:24:58 +0100

    Add questions for chemistry and physics

---
+++ Question.js
@@ -0,0 +1,9 @@
+const questions = {
+chemistry: [
+{question:"Water is composed of?", options:["Hydrogen and Oxygen","Nitrogen and Oxygen"], answer:"Hydrogen and Oxygen"}
+],
+
+physics: [
+{question:"SI unit of force?", options:["Newton","Joule"], answer:"Newton"}
+]
+};
